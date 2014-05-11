6d5c14bbd3608679437e5e67e079ac55938fc3c8
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2014-05-12 02:16:45 +1000

    Add exponential backoff

--- lib/retry.ex
+++ lib/retry.ex
@@ -30,6 +30,37 @@ defmodule Retry do
           run.(1, run)
         end
       end
+
+      defmacro backoff(timeout, do: block) do
+        quote do
+          run = fn(attempt, self) ->
+            # http://dthain.blogspot.com.au/2009/02/exponential-backoff-in-distributed.html
+            sleep = :erlang.round((1 + :random.uniform) * 10 * :math.pow(2, attempt))
+            IO.puts "Next sleep: #{sleep}"
+
+            if sleep <= unquote(timeout) do
+              IO.puts "Attempt #{attempt}"
+
+              try do
+                case unquote(block) do
+                  {:error, _} -> 
+                    :timer.sleep(sleep)
+                    self.(attempt + 1, self)
+                end
+              rescue
+                e in RuntimeError -> 
+                  :timer.sleep(sleep)
+                  self.(attempt + 1, self)
+              end
+            else
+              IO.puts "Giving up"
+              unquote(block)
+            end
+          end
+
+          run.(1, run)
+        end
+      end
     end
   end
 end
--- test/retry_test.exs
+++ test/retry_test.exs
@@ -2,7 +2,7 @@ defmodule RetryTest do
   use ExUnit.Case
   use Retry
 
-  test "should retry execution for specified attempts when result is error tuple" do
+  test "retry should retry execution for specified attempts when result is error tuple" do
     result = retry 5 in 500 do
       {:error, "Error"}
     end
@@ -10,9 +10,25 @@ defmodule RetryTest do
     assert result = {:error, "Error"}
   end
 
-  test "should retry execution for specified attempts when error is raised" do
+  test "retry should retry execution for specified attempts when error is raised" do
     assert_raise RuntimeError, fn ->
       retry 5 in 500 do
+        raise "Error"
+      end
+    end
+  end
+
+  test "backoff should retry execution for specified period when result is error tuple" do
+    result = backoff 1000 do
+      {:error, "Error"}
+    end
+
+    assert result = {:error, "Error"}
+  end
+
+  test "backoff should retry execution for specified period when error is raised" do
+    assert_raise RuntimeError, fn ->
+      backoff 1000 do
         raise "Error"
       end
     end
