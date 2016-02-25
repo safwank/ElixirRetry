f476254d18935af0dcf855f14fb5fb4349457002
Juan Pablo Pinilla <juan@kamikazepanda.com>
2016-02-25 12:59:43 -0500

    Bump version to v0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Retry.Mixfile do
 
   def project do
     [app: :retry,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
