fc1fa39b4b376988adff0d7b9f8ec38e4f09e0d8
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2016-05-05 05:30:15 +1000

    Add Hex details

--- mix.exs
+++ mix.exs
@@ -2,31 +2,33 @@ defmodule Retry.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :retry,
-     version: "0.1.0",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :retry,
+      name: "elixir_retry",
+      description: "Simple Elixir macros for linear retries and exponential backoffs.",
+      version: "0.1.0",
+      elixir: "~> 1.1",
+      source_url: "https://github.com/safwank/ElixirRetry",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: package
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      maintainers: ["Safwan Kamarrudin"],
+      licenses: ["Apache 2.0"],
+      links: %{github: "https://github.com/safwank/ElixirRetry"}
+    ]
   end
 end
