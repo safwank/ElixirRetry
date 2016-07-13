c3350ad1ed19600dd97816dc80c9d78e8c7b182b
Peter Williams <pezra@barelyenough.org>
2016-07-13 12:20:56 -0600

    move to composable stream based retry delay system

---
+++ lib/retry/delay_streams.ex
@@ -0,0 +1,131 @@
+defmodule Retry.DelayStreams do
+  @moduledoc """
+
+  This module provide a set of helper functions that produce delay streams for
+  use with `retry`.
+
+  """
+
+
+  @doc """
+
+  Returns a stream of delays that increase exponentially.
+
+  Example
+
+      retry exp_backoff do
+        # ...
+      end
+
+  """
+  def exp_backoff do
+    Stream.unfold(1, fn failures ->
+      {:erlang.round(10 * :math.pow(2, failures)), failures+1}
+    end )
+  end
+
+  @doc """
+
+  Returns a stream of delays that increase linearly.
+
+  Example
+
+      retry lin_backoff(@fibonacci) do
+        # ...
+      end
+
+  """
+  def lin_backoff(initial_delay, factor) do
+    Stream.unfold(initial_delay, fn last_delay ->
+      next_d = last_delay * factor
+
+      {next_d, next_d}
+    end )
+  end
+
+  @doc """
+
+  Returns a stream in which each element of `delays` is randomly adjusted no
+  more than `proportion` of the delay.
+
+  Example
+
+      retry exp_backoff |> randomize do
+        # ...
+      end
+
+
+  Produces an exponentially increasing delay stream where each delay is randomly
+  adjusted to be within 10 percent of the original value
+
+
+  """
+  def randomize(delays, proportion \\ 0.1) do
+    Stream.map(delays, fn d ->
+      max_delta = d * proportion |> round
+      shift = :rand.uniform( 2 * max_delta) - max_delta
+
+      d + shift
+    end )
+  end
+
+  @doc """
+
+  Returns a stream that is the same as `delays` except that the delays never
+  exceed `max`. This allow capping the delay between attempts to some max value.
+
+  Example
+
+      retry exp_backoff |> cap(10_000) do
+        # ...
+      end
+
+  Produces an exponentially increasing delay stream until the delay reaches 10
+  seconds at which point it stops increasing
+
+  """
+  def cap(delays, max) do
+    Stream.map(delays, fn d ->
+      if d <= max do
+        d
+      else
+        max
+      end
+    end )
+  end
+
+  @doc """
+
+  Returns a delay stream thatthat is the same as `delays` except it limits the
+  total life span of the stream to `time_budget`. This calculation take the
+  execution time of the block being retried into account.
+
+  Example
+
+      retry exp_backoff |> expiry(1000) do
+        # ...
+      end
+
+  Produces a delay stream that ends after 1 second has elapsed since its
+  creation.
+
+  """
+  def expiry(delays, time_budget) do
+    end_t = :os.system_time(:milli_seconds) + time_budget
+
+    Stream.transform(delays, :normal, fn preferred_delay, status ->
+      now_t = :os.system_time(:milli_seconds)
+      remaining_t = Enum.max([end_t - now_t, 0])
+
+      cond do
+        :at_end == status             # time expired!
+          -> {:halt, status}
+        preferred_delay > remaining_t  # one last try
+          ->  {[remaining_t], :at_end}
+       true
+          -> {[preferred_delay], status}
+      end
+
+    end )
+  end
+end
---
+++ test/retry/delay_streams_test.exs
@@ -0,0 +1,70 @@
+defmodule Retry.DelayStreamsTest do
+  use ExUnit.Case, async: true
+  import Retry.DelayStreams
+
+  test "exponential backoff" do
+    exp_backoff
+    |> Enum.take(5)
+    |> Enum.scan(fn (delay, last_delay) ->
+      assert delay > last_delay
+      delay
+    end )
+  end
+
+  test "lin_backoff/2" do
+    lin_backoff(10, 1.5)
+    |> Enum.take(5)
+    |> Enum.scan(fn (delay, last_delay) ->
+      assert (last_delay * 1.5) == delay
+      delay
+    end )
+  end
+
+  test "delay streams can be capped" do
+    exp_backoff
+    |> cap(1000)
+    |> Stream.take(10)
+    |> Enum.all?(&(&1 <= 1000))
+  end
+
+  test "expiry/1 limits life time" do
+    {elapsed, _} = :timer.tc fn ->
+      Stream.cycle([500])
+      |> expiry(1000)
+      |> Enum.each(&:timer.sleep(&1))
+    end
+
+    assert_in_delta elapsed/1000, 1000, 10
+  end
+
+  test "expiry/1 doesn't mess up delays" do
+    assert exp_backoff |> Enum.take(5) ==
+      exp_backoff |> expiry(1000) |> Enum.take(5)
+  end
+
+  test "ramdomize/1 randomizes streams" do
+    delays = Stream.cycle([500])
+    |> randomize
+    |> Enum.take(100)
+
+    Enum.each(delays, fn (delay) ->
+      assert_in_delta delay, 500, 500 * 0.1 + 1
+      delay
+    end )
+
+    assert Enum.any?(delays, &(&1 != 500))
+  end
+
+  test "ramdomize/2 randomizes streams" do
+    delays = Stream.cycle([500])
+    |> randomize(0.2)
+    |> Enum.take(100)
+
+    Enum.each(delays, fn (delay) ->
+      assert_in_delta delay, 500, 500 * 0.2 + 1
+      delay
+    end )
+
+    assert Enum.any?(delays, &(abs(&1 - 500) > (500 * 0.1)))
+  end
+end
