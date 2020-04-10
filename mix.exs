c3803b04d00f7e9906147b85a0edb73ca1b061f2
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2020-04-10 10:44:38 +1000

    Add support for Elixir 1.10

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule Retry.Mixfile do
       name: "retry",
       description:
         "Simple Elixir macros for linear retry, exponential backoff and wait with composable delays.",
-      version: "0.13.0",
-      elixir: "~> 1.7",
+      version: "0.14.0",
+      elixir: "~> 1.8",
       source_url: "https://github.com/safwank/ElixirRetry",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -33,11 +33,11 @@ defmodule Retry.Mixfile do
 
   defp deps do
     [
-      {:credo, "~> 1.0.5", only: :test},
-      {:excoveralls, "~> 0.11", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test]},
-      {:ex_doc, "~> 0.20", only: :dev},
-      {:earmark, "~> 1.3", only: :dev}
+      {:credo, "~> 1.3.0", only: :test},
+      {:excoveralls, "~> 0.12.0", only: :test},
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test]},
+      {:ex_doc, "~> 0.21.0", only: :dev},
+      {:earmark, "~> 1.4.0", only: :dev}
     ]
   end
 
