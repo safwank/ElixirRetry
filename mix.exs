a8d5e3c649eab46f724604786cb617c85549c0f2
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2025-03-02 12:47:01 +1100

    Bump Elixir and Erlang

--- mix.exs
+++ mix.exs
@@ -8,16 +8,12 @@ defmodule Retry.Mixfile do
       description:
         "Simple Elixir macros for linear retry, exponential backoff and wait with composable delays.",
       version: "0.19.0",
-      elixir: "~> 1.15",
-      source_url: "https://github.com/safwank/ElixirRetry",
-      docs: [
-        extras: ["README.md"],
-        source_ref: "master"
-      ],
-      build_embedded: Mix.env() == :prod,
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
+      docs: docs(),
+      dialyzer: dialyzer(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         credo: :test,
@@ -31,25 +27,36 @@ defmodule Retry.Mixfile do
     ]
   end
 
-  def application do
-    [applications: [:logger]]
-  end
-
   defp deps do
     [
-      {:credo, "~> 1.7.0", only: :test},
-      {:excoveralls, "~> 0.18.0", only: :test},
-      {:dialyxir, "~> 1.4.0", only: [:dev, :test]},
-      {:ex_doc, "~> 0.34.0", only: :dev},
-      {:earmark, "~> 1.4.0", only: :dev}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:earmark, "~> 1.4", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
       maintainers: ["Safwan Kamarrudin"],
-      licenses: ["Apache 2.0"],
-      links: %{github: "https://github.com/safwank/ElixirRetry"}
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/safwank/ElixirRetry"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_url: "https://github.com/safwank/ElixirRetry",
+      source_ref: "master"
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_add_apps: [:ex_unit, :mix],
     ]
   end
 end
